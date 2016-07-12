c6309dfe9dc9125810c17b937c3772d6a34bc84e
Daniel Asarnow <dasarnow@gmail.com>
2016-07-12 16:44:32 -0700

    Refactored in anticipation of parallelization.

--- projection_subtraction_v3.py
+++ projection_subtraction_v3.py
@@ -49,24 +49,16 @@ def main():
         output_star.write("_{0} #{1}\n".format(heading, i + 1))
 
     # Calculate projection subtraction and append output.star
-    count = 1
     for N in range(npart):
         if N % 100 == 0:
-            print "Projection-subtracted {0} of {1} particles...".format(
-                N, npart)
+            print "Projection-subtracted {0} of {1} particles...".format(N, npart)
         ptcl_n = int(star['rlnImageName'][N].split("@")[0]) - 1
         ptcl_name = star['rlnImageName'][N].split("@")[1]
         ptcl = EMData(ptcl_name, ptcl_n)
-
-        ctfproj = make_proj(N, dens, star)
-        ctfproj_sub = make_proj(N, sub_dens, star)
-
-        ptcl_sub = ptcl.process("math.sub.optimal", {"ref": ctfproj,
-                                                     "actual": ctfproj_sub})
-        ptcl_norm_sub = ptcl_sub.process("normalize")
+        meta = MetaData(star, N)
+        ptcl_sub, ptcl_norm_sub = subtract(ptcl, meta, dens, sub_dens)
         ptcl_norm_sub.write_image("{0}.mrcs".format(options.output), -1)
         ptcl.write_image("{0}_original.mrcs".format(options.output), -1)
-
         # Output for testing
         #      ptcl_sub_img = ptcl.process("math.sub.optimal", {"ref":ctfproj,
         #                             "actual":ctfproj_sub, "return_subim":True})
@@ -77,43 +69,49 @@ def main():
         #      ptcl_sub_lowpass.write_image("poreclass_sublowpass.mrcs", -1)
         #      ctfproj.write_image("poreclass_ctfproj.mrcs", -1)
         #      ctfproj_sub.write_image("poreclass_ctfprojsub.mrcs", -1)
-
         # Change image name and write output.star
-        star['rlnImageName'][N] = "{0:06d}@{1}".format(count,
-                                                       "{0}.mrcs".format(options.output))
+        star['rlnImageName'][N] = "{0:06d}@{1}".format(N+1, "{0}.mrcs".format(options.output))
         line = '  '.join(str(star[key][N]) for key in headings)
         output_star.write("{0}\n".format(line))
-        count += 1
 
 
-def make_proj(N, dens, star):
-    phi = star['rlnAngleRot'][N]
-    psi = star['rlnAnglePsi'][N]
-    theta = star['rlnAngleTilt'][N]
-    x_origin = star['rlnOriginX'][N]
-    y_origin = star['rlnOriginY'][N]
+def subtract(ptcl, meta, dens, sub_dens):
+    ctfproj = make_proj(dens, meta)
+    ctfproj_sub = make_proj(sub_dens, meta)
+    ptcl_sub = ptcl.process("math.sub.optimal", {"ref": ctfproj, "actual": ctfproj_sub})
+    ptcl_norm_sub = ptcl_sub.process("normalize")
+    return ptcl_sub, ptcl_norm_sub
 
+
+def make_proj(dens, meta):
     t = Transform()
-    t.set_rotation({'psi': psi, 'phi': phi, 'theta': theta, 'type': 'spider'})
-    t.set_trans(-x_origin, -y_origin)
-
+    t.set_rotation({'psi': meta.psi, 'phi': meta.phi, 'theta': meta.theta, 'type': 'spider'})
+    t.set_trans(-meta.x_origin, -meta.y_origin)
     proj = dens.project("standard", t)
-
-    # CTFFIND4 --> sparx CTF conventions (from CTER paper)
-    defocus = (star['rlnDefocusU'][N] + star['rlnDefocusV'][N]) / 20000.0
-    dfdiff = (star['rlnDefocusU'][N] - star['rlnDefocusV'][N]) / 10000.0
-    dfang = 90.0 - star['rlnDefocusAngle'][N]
-    apix = ((10000.0 * star['rlnDetectorPixelSize'][N]) /
-            float(star['rlnMagnification'][N]))
-    voltage = star["rlnVoltage"][N]
-    cs = star["rlnSphericalAberration"][N]
-    ac = star["rlnAmplitudeContrast"][N] * 100.0
-    bfactor = 0
-
-    ctf = generate_ctf([defocus, cs, voltage, apix, bfactor, ac, dfdiff, dfang])
+    ctf = generate_ctf(meta.ctf_params)
     ctf_proj = filt_ctf(proj, ctf)
-
     return ctf_proj
+
+
+class MetaData:
+    def __init__(self, star, N):
+        self.phi = star['rlnAngleRot'][N]
+        self.psi = star['rlnAnglePsi'][N]
+        self.theta = star['rlnAngleTilt'][N]
+        self.x_origin = star['rlnOriginX'][N]
+        self.y_origin = star['rlnOriginY'][N]
+        # CTFFIND4 --> sparx CTF conventions (from CTER paper)
+        self.defocus = (star['rlnDefocusU'][N] + star['rlnDefocusV'][N]) / 20000.0
+        self.dfdiff = (star['rlnDefocusU'][N] - star['rlnDefocusV'][N]) / 10000.0
+        self.dfang = 90.0 - star['rlnDefocusAngle'][N]
+        self.apix = ((10000.0 * star['rlnDetectorPixelSize'][N]) /
+                     float(star['rlnMagnification'][N]))
+        self.voltage = star["rlnVoltage"][N]
+        self.cs = star["rlnSphericalAberration"][N]
+        self.ac = star["rlnAmplitudeContrast"][N] * 100.0
+        self.bfactor = 0
+        self.ctf_params = [self.defocus, self.cs, self.voltage, self.apix, self.bfactor, self.ac, self.dfdiff,
+                           self.dfang]
 
 
 if __name__ == "__main__":
