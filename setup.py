189b7cf3cb26569d92c02ad1c589043a3850d42e
Daniel Asarnow <dasarnow@gmail.com>
2016-08-29 16:52:20 -0700

    setup.py installs scripts and requirements

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyem',
+    version='0.1',
+    packages=['pyem'],
+    scripts=['angdist', 'project', 'projection_subtraction', 'recenter', 'pyem/star'],
+    url='https://github.com/asarnow/pyem',
+    license='GNU Public License Version 3',
+    author='Daniel Asarnow',
+    author_email='dasarnow@gmail.com',
+    description='Python programs for electron microscopy',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'pathos']
+)
