04e47171193136c6b57aa8d03445159e518ee31d
Daniel Asarnow <dasarnow@gmail.com>
2017-01-18 18:05:06 -0800

    subset.py, new program for:  - Data subsetting  - Data resampling

---
+++ subset.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# Copyright (C) 2017 Daniel Asarnow
+# University of California, San Francisco
+#
+# Program for subsetting and resampling EM data.
+# See help text and README file for more information.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import sys
+from pyem.star import parse_star
+from pyem.star import write_star
+
+def main(args):
+    star = parse_star(args.input, keep_index=False)
+
+    if args.cls is not None:
+        clsfields = [f for f in star.columns if "ClassNumber" in f]
+        if len(clsfields) == 0:
+            print("No class labels found")
+            return 1
+        ind = star[clsfields[0]].isin(args.cls)
+        if not np.any(ind):
+            print("Specified class has no members")
+            return 1
+        star = star.loc[ind]
+
+    if args.max_astigmatism is not None:
+        astigmatism = star["rlnDefocusU"] - star["rlnDefocusV"]
+        ind = astigmatism <= args.max_astigmatism
+        star = star.loc[ind]
+
+    if args.max_resolution is not None:
+        ind = star["rlnFinalResolution"] <= args.max_resolution
+        star = star.loc[ind]
+
+    if args.min_ctf_fom is not None:
+        ind = star["rlnCtfFigureOfMerit"] >= args.min_ctf_fom
+        star = star.loc[ind]
+
+    write_star(args.output, star)
+    return 0
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--class", help="Keep this class in output, may be passed multiple times",
+                        action="append", type=int, dest="cls")
+    parser.add_argument("--max-astigmatism", help="Maximum astigmatism (defocus difference) in Angstroms",
+                        type=float)
+    parser.add_argument("--max-resolution", help="Maximum CTF resolution in Angstroms",
+                        type=float)
+    parser.add_argument("--min-ctf-fom", help="Minimum CTF figure-of-merit",
+                        type=float)
+    parser.add_argument("input", help="Input .star file")
+    parser.add_argument("output", help="Output .star file")
+    sys.exit(main(parser.parse_args()))
