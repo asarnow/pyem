24a26b18fbbecf3b518a058f3fc357ff27f087bc
Daniel Asarnow <dasarnow@gmail.com>
2017-02-23 13:07:50 -0800

    Groundwork for all recentering methods: * Recenter particles on particle phase CoM (no arguments) * Recenter particles on corresponding 2D class CoM (--class-2d) * Recenter particles on corresponding 3D class CoM (--class-3d) * Zero particle origins regardless of above methods (--zero-origin)
    TODO:
    * Fix transform code in transform_com()
    * Fix particle parsing in case no --class-2d or --class-3d
    * Check changes to DataFrame rows are preserved.

--- recenter.py
+++ recenter.py
@@ -1,34 +1,85 @@
 #! /usr/bin/python2.7
 # Copyright (C) 2016 Daniel Asarnow
 # University of California, San Francisco
+#
+# Program implementing several methods for recentering particles.
+# See help text and README file for more information.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import glob
 import logging
 import sys
 import numpy as np
-from pathos.multiprocessing import Pool
-from scipy.ndimage.interpolation import affine_transform
-from EMAN2 import EMData, Vec3f
+from star import parse_star, write_star
+from EMAN2 import EMData, Vec3f, Transform
 
 
 def main(args):
-    inp = glob.glob(args.input)
+    star = parse_star(args.input, keep_index=False)
 
-    pool = None
-    if args.nproc > 1:
-        pool = Pool(args.nproc)
-
+    if args.class_2d is not None:
+        refs = glob.glob(args.class_2d)
+    elif args.class_3d is not None:
+        refs = glob.glob(args.class_3d)
     else:
-        results = ()
+        refs = []
+    
+    shifts = []
+    for r in refs:
+        if args.class_3d is not None:
+            refmap = EMData(r)
+            com = Vec3f(*refmap.phase_cog()[:3])
+            shifts.append(com)
+        else:
+            stack = EMData.read_images(r)
+            for im in stack:
+                com = Vec2f(*im.phase_cog()[:2])
+                shifts.append(com)
+
+    if args.class_2d is None and args.class_3d is None:
+        for ptcl in star.rows:
+            im = EMData.read_image(ptcl)
+            com = im.phase_cog()
+            ptcl["rlnOriginX"] += com[0]
+            ptcl["rlnOriginY"] += com[1]
+    else:
+        for ptcl in star.rows:
+            com = shifts[ptcl["rlnClassNumber"]]
+            xshift, yshift = transform_com(com, ptcl)
+            ptcl["rlnOriginX"] += xshift
+            ptcl["rlnOriginY"] += yshift
+
+    if args.zero_origin:
+        star["rlnCoordinateX"] = star["rlnCoordinateX"] - star["rlnOriginX"]
+        star["rlnCoordinateY"] = star["rlnCoordinateY"] - star["rlnOriginY"]
+        star["rlnOriginX"] = 0
+        star["rlnOriginY"] = 0
+
+    write_star(args.output, star, reindex=True)
 
     return 0
 
 
-def recenter(inp, tr=None):
-    if "mrc" in inp[-4:]:
-        im = EMData(inp)
-        if im.get_ndim() == 3:
-            com = Vec3f(*im.phase_cog()[:3])
-            im.set_translation(-com)
+def transform_com(com, ptcl):
+    t = Transform()
+    if len(com) == 3:
+        t.set_rotation({'psi': meta.psi, 'phi': meta.phi, 'theta': meta.theta, 'type': 'spider'})
+    else:
+        t.set_rotation({'psi': meta.psi})
+    shift = t.transform(com)
+    # The change in the origin is the projection of the transformed difference vector on the new xy plane.
+    return shift[0], shift[1]
 
 
 def find_cm(im):
@@ -39,30 +90,15 @@ def find_cm(im):
     return mu_x, mu_y
 
 
-def recenter2d(im, cm, y=None):
-    if len(cm) == 1 and y is None:
-        t = np.array([[1, 0, cm], [0, 1, cm]])
-    elif len(cm) == 1 and len(y) == 1:
-        t = np.array([[1, 0, cm], [0, 1, y]])
-    else:
-        t = np.array([[1, 0, cm[0]], [0, 1, cm[1]]])
-    return affine_transform(im, t)
-
-
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--loglevel", "-l", help="Logging level")
-    parser.add_argument("--mask", "-m", help="Size of circular particle mask")
-    parser.add_argument("--nproc", "-n", help="Number of parallel processes")
-    parser.add_argument("--prefix", "-p", help="Prefix to be prepended to output file names")
-    parser.add_argument("--translations", "-t", help="File to note translation applied to each particle")
-    parser.add_argument("input", help="Input .star or .mrc(s) file(s)")
-    parser.add_argument("output", help="Output destination")
-
+    parser.add_argument("--class-2d", help="2D class images for recentering (pass glob in quotes for multiple files)")
+    parser.add_argument("--class-3d", help="3D class images for recentering (pass glob in quotes for multiple files)")
+    parser.add_argument("--zero-origin", help="Subtract particle origin from particle coordinates in output")
+    parser.add_argument("input", help="Input .star file")
+    parser.add_argument("output", help="Output .star file")
     args = parser.parse_args()
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.getLevelName(args.loglevel.upper()))
-
+    #logger = logging.getLogger(__name__)
+    #logger.setLevel(logging.getLevelName(args.loglevel.upper()))
     sys.exit(main(args))
