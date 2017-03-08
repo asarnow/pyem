1e928a81e1add89244fc795a916039eef7f0548c
Daniel Asarnow <dasarnow@gmail.com>
2017-03-07 18:10:27 -0800

    - Brief function docstrings - isrotation() - rot2euler()

--- pyem/util.py
+++ pyem/util.py
@@ -20,5 +20,37 @@
 import numpy as np
 
 def cent2edge(bins):
+    """Convert bin centers to bin edges"""
     return np.r_[-np.Inf, 0.5 * (bins[:-1] + bins[1:]), np.Inf]
+
+
+def isrotation(r, tol=1e-4):
+    """Check for valid rotation matrix"""
+    check = np.identity(3, dtype=r.dtype) - np.dot(r.T, r)
+    if tol is None:
+        return check
+    return np.linalg.norm(check) < tol
+
+
+def rot2euler(r):
+    """Decompose rotation matrix into Euler angles"""
+    assert(isrotation(r))
+
+    if np.abs(r[2,0]) != 1:
+        theta = -np.arcsin(r[2,0])
+        # theta2 = pi - theta
+        psi = np.arctan2(r[2,1] / np.cos(theta), r[2,2] / np.cos(theta));
+        # psi2 = np.arctan2(r[2,1] / np.cos(theta2), r[2,2] / np.cos(theta2));
+
+        phi = np.arctan2(r[1,0] / np.cos(theta), r[0,0] / np.cos(theta));
+        # phi2 = np.arctan2(r[1,0] / np.cos(theta2), r[0,0] / np.cos(theta2));
+    else:
+        phi = 0
+        if r[2,0] == -1:
+            theta = pi/2
+            psi = phi + np.arctan2(r[0,1], r[0,2])
+        else:
+            theta = -pi/2
+            psi = -phi + np.arctan2(-r[0,1], -r[0,2])
+    return psi, theta, phi
 
