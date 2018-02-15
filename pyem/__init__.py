0964c70e0e9eeb18a99204f6c0619b03d59306a5
Daniel Asarnow <dasarnow@gmail.com>
2018-02-14 16:01:48 -0800

    Trying to make __init__.py imports all work

--- pyem/__init__.py
+++ pyem/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2016-2018 Daniel Asarnow
 # University of Calfornia, San Francisco
-from star import parse_star
-from star import write_star
-from mrc import *
+from . import metadata
+from . import mrc
+from . import star
+from . import util
+from . import vop
 
--- pyem/vop/__init__.py
+++ pyem/vop/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2018 Daniel Asarnow
 # University of California, San Francisco
-from __future__ import print_function
-from vop import *
-from binary import *
-from vop_numba import *
+from .vop import *
+from .binary import *
+from .vop_numba import *
 
