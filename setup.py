d45694f3318a9e81ff8d5bc4fed3f3a5faadbe02
Daniel Asarnow <dasarnow@gmail.com>
2018-03-05 17:41:56 -0800

    Require pyfftw

--- setup.py
+++ setup.py
@@ -7,13 +7,11 @@ setup(
     name='pyem',
     version='0.1',
     packages=find_packages(),
-#    scripts=['angdist', 'project', 'projection_subtraction', 'recenter', 'pyem/star'],
-#    scripts=['pyem/star'],
     url='https://github.com/asarnow/pyem',
     license='GNU Public License Version 3',
     author='Daniel Asarnow',
     author_email='asarnow@msg.ucsf.edu',
     description='Python programs for electron microscopy',
-    install_requires=['future', 'numba', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'pathos'],
+    install_requires=['future', 'numba', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'pathos', 'pyfftw'],
     zip_safe=False
 )
