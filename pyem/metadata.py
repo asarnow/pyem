6e9d236bb04f506ad99016df763883dae09a6931
Daniel Asarnow <dasarnow@gmail.com>
2018-06-04 20:29:15 -0700

    metadata.py  - cryosparc_065_csv2star had phic block under phase shift if  - started cryosparc2 code

--- pyem/metadata.py
+++ pyem/metadata.py
@@ -170,26 +170,26 @@ def cryosparc_065_csv2star(meta, minphic=0):
     phic = None
     if "rlnPhaseShift" in df.columns:
         df["rlnPhaseShift"] = np.rad2deg(df["rlnPhaseShift"])
-        # Class assignments and other model parameters.
-        phic = meta[[h for h in meta.columns if
-                     "phiC" in h]]  # Posterior probability over class assignments.
-        if len(phic.columns) > 0:  # Check class assignments exist in input.
-            # phic.columns = [int(h[21]) for h in meta.columns if "phiC" in h]
-            phic.columns = range(len(phic.columns))
-            cls = phic.idxmax(axis=1)
-            for p in model:
-                if model[p] is not None:
-                    pspec = p.split("model")[1]
-                    param = meta[[h for h in meta.columns if pspec in h]]
-                    if len(param.columns) > 0:
-                        param.columns = phic.columns
-                        df[model[p]] = param.lookup(param.index, cls)
-            df["rlnClassNumber"] = cls + 1  # Add one for Relion indexing.
-        else:
-            for p in model:
-                if model[p] is not None and p in meta.columns:
-                    df[model[p]] = meta[p]
-            df["rlnClassNumber"] = 1
+    # Class assignments and other model parameters.
+    phic = meta[[h for h in meta.columns if
+                 "phiC" in h]]  # Posterior probability over class assignments.
+    if len(phic.columns) > 0:  # Check class assignments exist in input.
+        # phic.columns = [int(h[21]) for h in meta.columns if "phiC" in h]
+        phic.columns = range(len(phic.columns))
+        cls = phic.idxmax(axis=1)
+        for p in model:
+            if model[p] is not None:
+                pspec = p.split("model")[1]
+                param = meta[[h for h in meta.columns if pspec in h]]
+                if len(param.columns) > 0:
+                    param.columns = phic.columns
+                    df[model[p]] = param.lookup(param.index, cls)
+        df["rlnClassNumber"] = cls + 1  # Add one for Relion indexing.
+    else:
+        for p in model:
+            if model[p] is not None and p in meta.columns:
+                df[model[p]] = meta[p]
+        df["rlnClassNumber"] = 1
     if df.columns.intersection(star.Relion.ANGLES).size == len(star.Relion.ANGLES):
         df[star.Relion.ANGLES] = np.rad2deg(
             df[star.Relion.ANGLES].apply(lambda x: util.rot2euler(util.expmap(x)),
@@ -198,3 +198,31 @@ def cryosparc_065_csv2star(meta, minphic=0):
         mask = np.all(phic < minphic, axis=1)
         df.drop(df[mask].index, inplace=True)
     return df
+
+
+def parse_cryosparc_2_cs(csfile):
+    cs = np.load(csfile)
+    general = {u'uid': None,
+               u'ctf/accel_kv': "rlnVoltage",
+               u'ctf_params.mag': "rlnMagnification",
+               u'blob/psize_A': "rlnDetectorPixelSize",
+               u'ctf/ac': "rlnAmplitudeContrast",
+               u'ctf/cs_mm': "rlnSphericalAberration",
+               u'ctf/df1_A': "rlnDefocusU",
+               u'ctf/df2_A': "rlnDefocusV",
+               u'ctf/df_angle_rad': "rlnDefocusAngle",
+               u'ctf/phase_shift_rad': "rlnPhaseShift",
+               u'ctf/cross_corr_ctffind4': "rlnCtfFigureOfMerit",
+               u'ctf/ctf_fit_to_A': "rlnCtfMaxResolution",
+               u'blob/path': "rlnImageName",
+               u'blob/idx': None}
+    model = {u'alignments3D/split': "rlnRandomSubset"}
+
+    alignments = [n for n in cs.dtype.names if n.startswith("alignment")]
+    # Starts with "alignments3D/", "alignments2D/", or "alignments_class_N/".
+    phic = [n for n in cs.dtype.names if "class_posterior" in n]
+    if len(phic) > 0:
+        cls = np.argmax(cs[phic], axis=1)
+
+    meta = pd.DataFrame()
+    return meta
