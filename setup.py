e9c0d51061d6955111b753590871b8ded0bbbfe2
Daniel Asarnow <dasarnow@gmail.com>
2018-06-21 16:11:44 -0700

    Updated requirements and setup to reflect v0.3 devel

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='pyem',
-    version='0.1',
+    version='0.3',
     packages=find_packages(),
     url='https://github.com/asarnow/pyem',
     license='GNU Public License Version 3',
