3e7ed53934307327390307239203eaa6e97c2998
Daniel Asarnow <dasarnow@gmail.com>
2018-12-03 11:08:44 -0800

    geom imports convert_numba.

--- pyem/geom/__init__.py
+++ pyem/geom/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2018 Daniel Asarnow
 # University of California, San Francisco
-from .convert import *
+from .convert_numba import *
 from .geom import *
 from .quat_numba import *
 from .quat import normq
