2af8f4bd7b7c38beb5facdfae04f1ac6dba9a97a
Daniel Asarnow <dasarnow@gmail.com>
2019-02-02 16:38:26 -0800

    added healpy to setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import find_packages
 
 setup(
     name='pyem',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     url='https://github.com/asarnow/pyem',
     license='GNU Public License Version 3',
     author='Daniel Asarnow',
     author_email='asarnow@msg.ucsf.edu',
     description='Python programs for electron microscopy',
-    install_requires=['future', 'numba', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'pathos', 'pyfftw'],
+    install_requires=['future', 'numba', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'pathos', 'pyfftw', 'healpy'],
     zip_safe=False
 )
