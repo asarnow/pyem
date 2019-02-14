90a544814de9ba33aa2358c5f8bcd13212b99ee4
Daniel Asarnow <dasarnow@gmail.com>
2019-02-13 20:20:40 -0800

    Remove non-numba quaternion functions.

--- pyem/geom/quat.py
+++ pyem/geom/quat.py
@@ -17,38 +17,6 @@ import numpy as np
 from .geom_numba import cross3
 
 
-def _qconj(q, p):
-    p[0] = q[0]
-    p[1] = -q[1]
-    p[2] = -q[2]
-    p[3] = -q[3]
-    return p
-
-
-qconj = np.vectorize(_qconj, signature="(m),(m)->(m)")
-
-
-def _qtimes(q1, q2, q3):
-    q3[0] = q1[0] * q2[0] - (q1[1] * q2[1] + q1[2] * q2[2] + q1[3] * q2[3])
-    q3[1] = q1[2] * q2[3] - q1[3] * q2[2] + q1[0] * q2[1] + q2[0] * q1[1]
-    q3[2] = q1[3] * q2[1] - q1[1] * q2[3] + q1[0] * q2[2] + q2[0] * q1[2]
-    q3[3] = q1[1] * q2[2] - q1[2] * q2[1] + q1[0] * q2[3] + q2[0] * q1[3]
-    return q3
-
-
-qtimes = np.vectorize(_qtimes, signature="(m),(m)->(m)")
-
-
-def _qsqrt(q, p):
-    p[0] = q[0] + 1
-    p[1:] = q[1:]
-    p[:] = p[:] / np.sqrt(2 * (1 + q[0]))
-    return p
-
-
-qsqrt = np.vectorize(_qsqrt, signature="(m),(m)->(m)")
-
-
 def qrotate(q, v):
     return v + cross3(2 * q[:, 1:], cross3(q[:, 1:], v) + q[:, 0].reshape(-1, 1) * v)
 
