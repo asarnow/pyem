Correct formula for maximum shifts (really).

project.py
@@ -50,8 +50,9 @@ def main(args):
     if args.size is None:
         args.size = vol.shape[0]
 
-    if args.size < (np.round(np.max(np.abs(df[star.Relion.ORIGINS].values))) + args.crop // 2):
-        log.error("Some shifts are too large to crop")
+    maxshift = np.round(np.max(np.abs(df[star.Relion.ORIGINS].values)))
+    if args.size // 2 < maxshift + args.crop // 2:
+        log.error("Some shifts are too large to crop (maximum crop is %d" % args.size - 2 * maxshift)
         return 1
 
     if args.subtract and args.size != vol.shape[0]: