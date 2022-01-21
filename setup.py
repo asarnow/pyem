581a758dcbf123cd335a57feeec60443e9e516cb
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2022-01-21 09:26:17 +0100

    add entrypoin_prefix variable

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
+entrypoint_prefix= "pyem_"
 setup(
     name='pyem',
     version='0.4',
@@ -17,26 +18,26 @@ setup(
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'pyem_csparc2star.py = scripts.csparc2star:_main_',
-            'pyem_star.py = scripts.star:_main_',
-            'pyem_recenter.py = scripts.recenter:_main_',
-            'pyem_angdist.py = scripts.angdist:_main_',
-            'pyem_cfsc.py = scripts.cfsc:_main_',
-            'pyem_ctf2star.py = scripts.ctf2star:_main_',
-            'pyem_emcalc.py = scripts.emcalc:_main_',
-            'pyem_map.py = scripts.map:_main_',
-            'pyem_mask.py = scripts.mask:_main_',
-            'pyem_par2star.py = scripts.par2star:_main_',
-            'pyem_pose.py = scripts.pose:_main_',
-            'pyem_project.py = scripts.project:_main_',
-            'pyem_projection_subtraction.py = scripts.projection_subtraction:_main_',
-            'pyem_reconstruct.py = scripts.reconstruct:_main_',
-            'pyem_sort.py = scripts.sort:_main_',
-            'pyem_stack.py = scripts.stack:_main_',
-            'pyem_star2bild.py = scripts.star2bild:_main_',
-            'pyem_subparticles.py = scripts.subparticles:_main_',
-            'pyem_subset.py = scripts.subset:_main_',
-            'pyem_varmap.py = scripts.varmap:_main_',
+            f'{entrypoint_prefix}csparc2star.py = scripts.csparc2star:_main_',
+            f'{entrypoint_prefix}star.py = scripts.star:_main_',
+            f'{entrypoint_prefix}recenter.py = scripts.recenter:_main_',
+            f'{entrypoint_prefix}angdist.py = scripts.angdist:_main_',
+            f'{entrypoint_prefix}cfsc.py = scripts.cfsc:_main_',
+            f'{entrypoint_prefix}ctf2star.py = scripts.ctf2star:_main_',
+            f'{entrypoint_prefix}emcalc.py = scripts.emcalc:_main_',
+            f'{entrypoint_prefix}map.py = scripts.map:_main_',
+            f'{entrypoint_prefix}mask.py = scripts.mask:_main_',
+            f'{entrypoint_prefix}par2star.py = scripts.par2star:_main_',
+            f'{entrypoint_prefix}pose.py = scripts.pose:_main_',
+            f'{entrypoint_prefix}project.py = scripts.project:_main_',
+            f'{entrypoint_prefix}projection_subtraction.py = scripts.projection_subtraction:_main_',
+            f'{entrypoint_prefix}reconstruct.py = scripts.reconstruct:_main_',
+            f'{entrypoint_prefix}sort.py = scripts.sort:_main_',
+            f'{entrypoint_prefix}stack.py = scripts.stack:_main_',
+            f'{entrypoint_prefix}star2bild.py = scripts.star2bild:_main_',
+            f'{entrypoint_prefix}subparticles.py = scripts.subparticles:_main_',
+            f'{entrypoint_prefix}subset.py = scripts.subset:_main_',
+            f'{entrypoint_prefix}varmap.py = scripts.varmap:_main_',
 
         ]
         }
