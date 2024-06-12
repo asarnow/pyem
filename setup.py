a788babae8ae89db92cbd9231bd8a95bfa0af3a4
Daniel Asarnow <asarnow@msg.ucsf.edu>
2024-06-12 15:26:48 -0700

    Removed future - python3 only now

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     author='Daniel Asarnow',
     author_email='asarnow@msg.ucsf.edu',
     description='Python programs for electron microscopy',
-    install_requires=['future', 'numba', 'numpy', 'scipy', 'matplotlib',
-                      'seaborn', 'pandas', 'pathos', 'pyfftw', 'healpy',
-                      'natsort', 'starfile'],
+    install_requires=['numba', 'numpy', 'scipy', 'matplotlib',
+                      'seaborn', 'pandas', 'pathos', 'pyfftw',
+                      'healpy', 'natsort', 'starfile'],
     zip_safe=False
 )
