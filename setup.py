44a10515cccf472b9170b8e2929ff17758a428a0
Daniel Asarnow <asarnow@msg.ucsf.edu>
2024-07-15 16:13:58 -0700

    Add numexpr to pip reqs

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     author='Daniel Asarnow',
     author_email='asarnow@msg.ucsf.edu',
     description='Python programs for electron microscopy',
-    install_requires=['numba', 'numpy', 'scipy', 'matplotlib',
-                      'seaborn', 'pandas', 'pathos', 'pyfftw',
-                      'healpy', 'natsort', 'starfile'],
+    install_requires=['numba', 'numpy', 'numexpr', 'scipy', 'matplotlib',
+                      'seaborn', 'pandas', 'pathos', 'pyfftw', 'healpy',
+                      'natsort', 'starfile'],
     zip_safe=False,
     entry_points={
         'console_scripts': [
